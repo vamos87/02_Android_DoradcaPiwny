f3e9e49cfe2ae05aa35106795d81c8fc98796f11
vamos87 <adrian.lawrentjew@gmail.com>
2019-02-15 19:32:21 +0100

    final commit

---
+++ app/src/main/java/com/example/doradcapiwny/FindBeerActivity.java
@@ -0,0 +1,27 @@
+package com.example.doradcapiwny;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class FindBeerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_beer);
+    }
+
+    public void onClickFindBeer(View view) {
+        TextView brands = (TextView) findViewById(R.id.brands);
+        Spinner color = (Spinner) findViewById(R.id.color);
+
+        String beerType = String.valueOf(color.getSelectedItem());
+        List<String> brandsList = new BeerExpert().getBrands(beerType);
+        brands.setText(brandsList.toString());
+    }
+}
